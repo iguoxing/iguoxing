d4bc098f3842cbe7d2bb6862a90212dd6e8d3052
ardenzhao <iguoxing@163.com>
2022-05-28 16:14:41 +0800

    Deploy website - based on c402e7fc7ff4850cd0d638a8ffdabc645329b471

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/zhaoguoxing/Downloads/projects/ardenblog/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/zhaoguoxing/Downloads/projects/ardenblog/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/zhaoguoxing/Downloads/projects/ardenblog/my-website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/zhaoguoxing/Downloads/projects/ardenblog/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/zhaoguoxing/Downloads/projects/ardenblog/my-website/src/css/custom.css'),
-];
